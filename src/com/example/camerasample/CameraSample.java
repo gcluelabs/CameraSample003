54aec2d206d3db035e8f69242440c053862891d4
gcluelabs <gcluelabs@gclue.jp>
2013-05-25 16:55:20 +0900

    パッケージ名の変更

--- src/com/example/camerasample/CameraSample.java
+++ src/com/example/camerasample/CameraSample.java
@@ -1,4 +1,4 @@
-package com.gclue.CameraSample;
+package com.example.camerasample;
 
 import android.app.Activity;
 import android.content.Context;
@@ -25,9 +25,7 @@ public class CameraSample extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		// Notification Barを消す
 		getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
-		// Title Barを消す
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 
 		CameraView mCamera = new CameraView(this);
@@ -42,9 +40,6 @@ public class CameraSample extends Activity {
  * CameraView
  */
 class CameraView extends SurfaceView implements SurfaceHolder.Callback {
-	/**
-	 * Cameraのインスタンスを格納する変数
-	 */
 	private Camera mCamera;
 
 	public CameraView(Context context) {
@@ -53,30 +48,20 @@ class CameraView extends SurfaceView implements SurfaceHolder.Callback {
 		getHolder().setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
 	}
 
-	/**
-	 * Surfaceに変化があった場合に呼ばれる
-	 */
 	@Override
 	public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
 		Log.i("CAMERA", "surfaceChaged");
 
-		// 画面設定
 		Camera.Parameters parameters = mCamera.getParameters();
 		parameters.setPreviewSize(width, height);
 		mCamera.setParameters(parameters);
-
-		// プレビュー表示を開始
 		mCamera.startPreview();
 	}
 
-	/**
-	 * Surfaceが生成された際に呼ばれる
-	 */
 	@Override
 	public void surfaceCreated(SurfaceHolder holder) {
 		Log.i("CAMERA", "surfaceCreated");
 
-		// カメラをOpen
 		mCamera = Camera.open();
 		try {
 			mCamera.setPreviewDisplay(holder);
@@ -84,74 +69,44 @@ class CameraView extends SurfaceView implements SurfaceHolder.Callback {
 		}
 	}
 
-	/**
-	 * Surfaceが破棄された場合に呼ばれる
-	 */
 	@Override
 	public void surfaceDestroyed(SurfaceHolder holder) {
 		Log.i("CAMERA", "surfaceDestroyed");
 
-		// カメラをClose
 		mCamera.stopPreview();
 		mCamera.release();
 		mCamera = null;
 	}
 }
 
-/**
- * オーバーレイ描画用のクラス
- */
 class MyView extends View {
 	
-	/**
-	 * x座標
-	 */
 	private int x;
 	
-	/**
-	 * y座標
-	 */
 	private int y;
 		
-	/**
-	 * コンストラクタ
-	 * 
-	 * @param context
-	 */
 	public MyView(Context context) {
 		super(context);
 		setFocusable(true);
 	}
-
-	/**
-	 * 描画処理
-	 */
 	protected void onDraw(Canvas canvas) {
 		super.onDraw(canvas);
 
-		// 背景色を設定 
 		canvas.drawColor(Color.TRANSPARENT);
 
-		// 描画するための線の色を設定 
 		Paint mainPaint = new Paint();
 		mainPaint.setStyle(Paint.Style.FILL);
 		mainPaint.setARGB(255, 255, 255, 100);
 
 	
-		// 線で描画
 		canvas.drawLine(x, y, 50, 50, mainPaint);
 	}
 	
-	/**
-	 * タッチイベント
-	 */
 	public boolean onTouchEvent(MotionEvent event) {
 		
-		// X,Y座標の取得 
 		x = (int)event.getX();		 
 		y = (int)event.getY();
 		
-		// 再描画の指示 
 		invalidate();
  
 		return true;
